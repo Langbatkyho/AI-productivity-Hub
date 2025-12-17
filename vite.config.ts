d63cb515d71517e762c2bf997ec34de5461ecaf9
Langbatkyho <36450563+Langbatkyho@users.noreply.github.com>
2025-12-17 10:45:17 +0700

    fix lỗi deploy lên Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      // Expose API_KEY to the client-side code as process.env.API_KEY
+      // It will fallback to VITE_API_KEY if API_KEY is not set
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY)
+    }
+  };
 });
